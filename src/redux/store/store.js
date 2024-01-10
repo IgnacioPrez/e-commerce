023556a416b998e1cfb23b3a2242187c7bdd57dc
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2024-01-09 21:27:35 -0300

    remove persist cart

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,11 +2,9 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import userReducer from '../slices/userSlices'
-import cartReducer from '../slices/cartSlices'
 
 const reducers = combineReducers({
-  user: userReducer,
-  cart: cartReducer
+  user: userReducer
 })
 
 const persistConfig = {
