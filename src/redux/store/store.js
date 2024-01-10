70e3975e37c6682950fa00c490143fe5949dce9d
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2024-01-09 21:55:55 -0300

    problems in redux

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,15 +2,16 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import userReducer from '../slices/userSlices'
-
+import cartReducer from '../slices/cartSlices'
 const reducers = combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 })
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['user', 'cart']
+  whitelist: ['user']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
